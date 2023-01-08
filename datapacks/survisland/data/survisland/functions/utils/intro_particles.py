75676e1aa23ead000329bac044917490386feeb8
Stoupy51 <stoupy51@gmail.com>
2023-01-08 16:08:58 +0100

    Intro plus petite

--- datapacks/survisland/data/survisland/functions/utils/intro_particles.py
+++ datapacks/survisland/data/survisland/functions/utils/intro_particles.py
@@ -3,7 +3,7 @@ import math
 
 a = 0.1
 b = 0.1
-r = 16
+r = 8
 
 stepSize = 0.06
 pos = []
@@ -34,11 +34,11 @@ for vert, rose, rouge, violet, jaune in pos:
 	x3, z3 = rouge
 	x4, z4 = violet
 	x5, z5 = jaune
-	f.write(f"execute if score #saison survisland.data matches 09 if score #intro survisland.data matches {t} run particle dust 0 0001 0000 3 ~{x1} ~{round(t/20,3)} ~{z1} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
-	f.write(f"execute if score #saison survisland.data matches 09 if score #intro survisland.data matches {t} run particle dust 1 .078 .136 3 ~{x2} ~{round(t/20,3)} ~{z2} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
-	f.write(f"execute if score #saison survisland.data matches 10 if score #intro survisland.data matches {t} run particle dust 1 0000 0000 3 ~{x3} ~{round(t/20,3)} ~{z3} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
-	f.write(f"execute if score #saison survisland.data matches 10 if score #intro survisland.data matches {t} run particle dust 1 0000 0001 3 ~{x4} ~{round(t/20,3)} ~{z4} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
-	f.write(f"execute if score #saison survisland.data matches 10 if score #intro survisland.data matches {t} run particle dust 1 0001 0000 3 ~{x5} ~{round(t/20,3)} ~{z5} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
+	f.write(f"execute if score #saison survisland.data matches 09 if score #intro survisland.data matches {t} run particle dust 0 0001 0000 3 ~{x1} ~{round(t/40,3)} ~{z1} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
+	f.write(f"execute if score #saison survisland.data matches 09 if score #intro survisland.data matches {t} run particle dust 1 .078 .136 3 ~{x2} ~{round(t/40,3)} ~{z2} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
+	f.write(f"execute if score #saison survisland.data matches 10 if score #intro survisland.data matches {t} run particle dust 1 0000 0000 3 ~{x3} ~{round(t/40,3)} ~{z3} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
+	f.write(f"execute if score #saison survisland.data matches 10 if score #intro survisland.data matches {t} run particle dust 1 0000 0001 3 ~{x4} ~{round(t/40,3)} ~{z4} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
+	f.write(f"execute if score #saison survisland.data matches 10 if score #intro survisland.data matches {t} run particle dust 1 0001 0000 3 ~{x5} ~{round(t/40,3)} ~{z5} 0.1 0.1 0.1 0 10 force @a[distance=0..]\n")
 	t += 1
 
 f.write(f"\n\n")
