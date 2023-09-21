1f5747ed6de50735829b06a252d3b7408a8172d5
Stoupy51 <stoupy51@gmail.com>
2023-09-21 23:49:09 +0200

    Sidebar fix

--- datapacks/survisland/data/survisland/functions/modes/cache_s13/u_sidebar_alives.py
+++ datapacks/survisland/data/survisland/functions/modes/cache_s13/u_sidebar_alives.py
@@ -2,7 +2,7 @@
 file = open("update_sidebar_alives.mcfunction", "w")
 file.write("\n")
 
-for i in range(20):
+for i in range(40):
 	file.write(f"""execute if score #specs_restants survisland.data matches {i} run team modify survisland.temp.sidebar.7 suffix [{{"text":"Specs restants : ","color":"gray"}},{{"text":"{i}","color":"white"}}]\n""")
 	file.write(f"""execute if score #mSambor_restants survisland.data matches {i} run team modify survisland.temp.sidebar.5 suffix [{{"text":"Joueurs restants : ","color":"white"}},{{"text":"{i}","color":"yellow"}}]\n""")
 	file.write(f"""execute if score #mKanawa_restants survisland.data matches {i} run team modify survisland.temp.sidebar.2 suffix [{{"text":"Joueurs restants : ","color":"dark_gray"}},{{"text":"{i}","color":"yellow"}}]\n""")
