676e7e1daeca51db2ca64d9693cab19552c509f7
Stoupy51 <stoupy51@gmail.com>
2024-12-20 00:20:45 +0100

    feat: ✨ More font

--- user/database/additions.py
+++ user/database/additions.py
@@ -28,8 +28,7 @@ def main(config: dict) -> dict[str, dict]:
 					"ground": { "scale": [0.5, 0.5, 0.5] },
 					"head": { "scale": [1.1, 1.1, 1.1] },
 				},
-			},
-			RESULT_OF_CRAFTING: [{"type":"crafting_shapeless","result_count":1,"ingredients":[ingr_repr("parchemin", ns=namespace)] }]
+			}
 		},
 		"deployed_parchemin": {
 			"id": "minecraft:warped_fungus_on_a_stick",
--- user/utils/screamer.py
+++ user/utils/screamer.py
@@ -10,22 +10,30 @@ def main(config: dict) -> None:
 	build_resource_pack: str = config["build_resource_pack"]
 	assets_folder: str = config["assets_folder"]
 
+	# Constants
+	SCREAMER_TEXT: str = "S"
+
 	# Make a font
 	font_providers: dict[str, list[dict]] = {
 		"providers": [
-			{"type":"bitmap","file":f"{namespace}:font/screamer.png","ascent":80,"height":160,"chars":["S"]}
+			{"type":"bitmap","file":f"{namespace}:font/shazinho.png","ascent":80,"height":160,"chars":[SCREAMER_TEXT]}
 		]
 	}
-	write_to_file(f"{build_resource_pack}/assets/{namespace}/font/screamer.json", super_json_dump(font_providers))
+	write_to_file(f"{build_resource_pack}/assets/{namespace}/font/screamers.json", super_json_dump(font_providers))
 
 	# Copy textures
-	super_copy(f"{assets_folder}/screamer.png", f"{build_resource_pack}/assets/{namespace}/textures/font/screamer.png")
+	super_copy(f"{assets_folder}/shazinho.png", f"{build_resource_pack}/assets/{namespace}/textures/font/shazinho.png")
 
-	# Make a function to make the screamer
-	write_to_function(config, f"{namespace}:utils/pas_touche", f"""
+	# Make a function to make the shazinho screamer
+	write_to_function(config, f"{namespace}:utils/pas_touche_1", f"""
 title @s times 0 0 40
-title @s title {{"text":"S","font":"{namespace}:screamer"}}
+title @s title {{"text":"{SCREAMER_TEXT}","font":"{namespace}:screamers"}}
 execute at @s run playsound {namespace}:vine_boom
+""")
+	write_to_function(config, f"{namespace}:utils/pas_touche_2", f"""
+title @s times 0 0 40
+title @s title {{"text":"{SCREAMER_TEXT}","font":"{namespace}:screamers"}}
+execute at @s run playsound {namespace}:scary_screamer
 """)
 
 	pass
