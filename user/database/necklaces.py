10ed4ec9c2449c18127b90c8ad380fbb34912772
Stoupy51 <stoupy51@gmail.com>
2025-07-02 15:21:40 +0200

    build: 🚀 Migrated to StewBeet

--- user/database/necklaces.py
+++ user/database/necklaces.py
@@ -1,18 +1,18 @@
 
 # Imports
-import stouputils as stp
-from python_datapack.utils.database_helper import *
+from stewbeet import CUSTOM_ITEM_VANILLA, Equipment, Mem, Texture, set_json_encoder
 
 # Constants
 NECKLACES: list[str] = ["necklace", "cursed_necklace", "dinosaur_necklace"]
 
-# Main function should return a database
-def main(config: dict, database: dict[str, dict]) -> None:
-	ns: str = config["namespace"]
+# Main function
+def main() -> None:
+	ns: str = Mem.ctx.project_id
+	textures_folder: str = Mem.ctx.meta.get("stewbeet", {}).get("textures_folder", "")
 
 	# Add each necklace
 	for necklace in NECKLACES:
-		database[necklace] = {
+		Mem.definitions[necklace] = {
 			"id": CUSTOM_ITEM_VANILLA,
 			"max_stack_size": 1,
 			"equippable": {
@@ -23,12 +23,10 @@ def main(config: dict, database: dict[str, dict]) -> None:
 		}
 
 		# Copy texture in the humanoid equipment folder
-		source: str = f"{config['assets_folder']}/textures/{necklace}_held.png"
-		destination: str = f"{config['build_resource_pack']}/assets/{ns}/textures/entity/equipment/humanoid/{necklace}_held.png"
-		super_copy(source, destination)
+		source: str = f"{textures_folder}/{necklace}_held.png"
+		Mem.ctx.assets[ns].textures[f"entity/equipment/humanoid/{necklace}_held"] = Texture(source_path=source)
 
 		# Write model
-		model_file: str = f"{config['build_resource_pack']}/assets/{ns}/equipment/{necklace}.json"
 		model_data: dict = {"layers": {"humanoid": [{"texture": f"{ns}:{necklace}_held"}]}}
-		write_file(model_file, stp.super_json_dump(model_data))
+		Mem.ctx.assets[ns].equipments[necklace] = set_json_encoder(Equipment(model_data))
 
--- user/utils/item_modifiers.py
+++ user/utils/item_modifiers.py
@@ -1,41 +1,38 @@
 
 # Imports
-import stouputils as stp
-from python_datapack.constants import *
-from python_datapack.utils.io import *
+from stewbeet import ItemModifier, Mem, set_json_encoder
+
 
 # Generates item modifiers
-def main(config: dict) -> None:
-	namespace: str = config["namespace"]
-	modifiers: str = f"{config['build_datapack']}/data/{namespace}/item_modifier"
-	database: dict[str, dict] = config['database']
+def main() -> None:
+	ns: str = Mem.ctx.project_id
 
 	# For each book, generate an item modifier to change item_model
-	for item, data in database.items():
+	for item, data in Mem.definitions.items():
 		if "book_" in item:
 			model: str = data["item_model"]
 			color: str = item.replace("book_", "")
 			item_modifier: dict = {"function":"minecraft:set_components","components":{"minecraft:item_model":model}}
-			write_file(f"{modifiers}/books/{color}.json", stp.super_json_dump(item_modifier))
+			Mem.ctx.data[ns].item_modifiers[f"books/{color}"] = set_json_encoder(ItemModifier(item_modifier))
 
 	# Switch scroll state
-	normal: str = database["parchemin"]["item_model"]
-	deployed: str = database["deployed_parchemin"]["item_model"]
+	normal: str = Mem.definitions["parchemin"]["item_model"]
+	deployed: str = Mem.definitions["deployed_parchemin"]["item_model"]
 	item_modifier: list[dict] = [
 		{"function":"minecraft:set_components","components":{"minecraft:item_model":normal},"conditions":[{"condition":"minecraft:entity_properties","entity":"this","predicate":{"equipment":{"mainhand":{"components":{"minecraft:item_model":deployed}}}}}]},
 		{"function":"minecraft:set_components","components":{"minecraft:item_model":deployed},"conditions":[{"condition":"minecraft:entity_properties","entity":"this","predicate":{"equipment":{"mainhand":{"components":{"minecraft:item_model":normal}}}}}]},
 	]
-	write_file(f"{modifiers}/switch_scroll_state.json", stp.super_json_dump(item_modifier, max_level = -1))
+	Mem.ctx.data[ns].item_modifiers["switch_scroll_state"] = set_json_encoder(ItemModifier(item_modifier), max_level=-1)
 
 	# Switch flambeau state
-	normal: str = database["flambeau"]["item_model"]
+	normal: str = Mem.definitions["flambeau"]["item_model"]
 	lighted: str = f"{normal}_on"
 	for slot in ["mainhand", "offhand"]:
 		item_modifier: list[dict] = [
 			{"function":"minecraft:set_components","components":{"minecraft:item_model":normal},"conditions":[{"condition":"minecraft:entity_properties","entity":"this","predicate":{"equipment":{slot:{"components":{"minecraft:item_model":lighted}}}}}]},
 			{"function":"minecraft:set_components","components":{"minecraft:item_model":lighted},"conditions":[{"condition":"minecraft:entity_properties","entity":"this","predicate":{"equipment":{slot:{"components":{"minecraft:item_model":normal}}}}}]},
 		]
-		write_file(f"{modifiers}/switch_flambeau_state/{slot}.json", stp.super_json_dump(item_modifier, max_level = -1))
+		Mem.ctx.data[ns].item_modifiers[f"switch_flambeau_state/{slot}"] = set_json_encoder(ItemModifier(item_modifier), max_level=-1)
 
 	pass
 
