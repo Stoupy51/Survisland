b84a717c3df539df99993632549850d7f473ee85
Stoupy51 <stoupy51@gmail.com>
2025-12-16 19:30:59 +0100

    refactor: ♻️ Refactor upload.py to include github_config directly and improve credential loading

--- upload.py
+++ upload.py
@@ -1,13 +1,22 @@
 
+# pyright: reportUnknownVariableType=false
 # Imports
+from beet import ProjectConfig
+from stewbeet import JsonDict
 from stewbeet.continuous_delivery import load_credentials, upload_to_github
+from stewbeet.utils import get_project_config
 
-from continuous_delivery.github_config import github_config
-
-# Get credentials
+# Get credentials and try to find the beet configuration
 credentials: dict[str, str] = load_credentials("~/stewbeet/credentials.yml")
+cfg: ProjectConfig = get_project_config()
 
 ## Uploads
 # Upload to GitHub
+github_config: JsonDict = {
+    "project_name": cfg.name,
+    "version": cfg.version,
+    "build_folder": cfg.output,
+    "endswith": [".zip"]
+}
 changelog: str = upload_to_github(credentials, github_config)
 
