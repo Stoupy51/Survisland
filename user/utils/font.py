da7e0d37cf864b8b932fb367609c912d78757cb8
Stoupy51 <stoupy51@gmail.com>
2026-04-13 22:33:14 +0200

    Ah

--- user/utils/font.py
+++ user/utils/font.py
@@ -9,24 +9,21 @@ def main() -> None:
 	assets_folder: str = f"{Mem.ctx.directory}/assets"
 
 	# Constants
-	SCREAMER_SHAZ: str = "1"
 	SCREAMER_STOUP: str = "2"
 
 	# Make a font
 	font_providers: dict[str, list[JsonDict]] = {
 		"providers": [
-			{"type":"bitmap","file":f"{ns}:font/shazinho.png","ascent":80,"height":160,"chars":[SCREAMER_SHAZ]},
 			{"type":"bitmap","file":f"{ns}:font/stoupinou.png","ascent":80,"height":160,"chars":[SCREAMER_STOUP]},
 		]
 	}
 	Mem.ctx.assets[ns].fonts["screamers"] = set_json_encoder(Font(font_providers))
 
 	# Copy textures
-	Mem.ctx.assets[ns].textures["font/shazinho"] = Texture(source_path=f"{assets_folder}/shazinho.png")
 	Mem.ctx.assets[ns].textures["font/stoupinou"] = Texture(source_path=f"{assets_folder}/stoupinou.png")
 
-	# Make a function to make the shazinho screamer
-	for text, name in [(SCREAMER_SHAZ, "shaz"), (SCREAMER_STOUP, "stoup")]:
+	# Make a function to make the stoupinou screamer
+	for text, name in [(SCREAMER_STOUP, "stoup")]:
 		write_function(f"{ns}:utils/s/{name}_vine", f"""
 title @s times 0 0 40
 title @s title {{"text":"{text}","font":"{ns}:screamers"}}
