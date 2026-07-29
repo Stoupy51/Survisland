3a9366218ad54b671543592782ce8ac48824c397
Stoupy51 <stoupy51@gmail.com>
2026-07-29 15:18:38 +0200

    perf: ⚡️ Optimized models and textures sizes

--- assets/optimize_textures.py
+++ assets/optimize_textures.py
@@ -1,37 +1,32 @@
 
-# Get start time & Enable colors in Windows 10 console
-from python_datapack.utils.print import *
+# Imports
 import os
-import time
-START_TIME = time.perf_counter()
-os.system("color")
+
+import stouputils as stp
+from PIL import Image
 
 # For each texture in the textures folder, optimize it without loosing any quality
-from PIL import Image
-for root, _, files in os.walk("./"):
-	for file in files:
-		if not file.endswith(".png"):
-			continue
-		filepath = f"{root}/{file}"
+with stp.MeasureTime(message="Textures optimized"):
+	for root, _, files in os.walk("./"):
+		for file in files:
+			if not file.endswith(".png"):
+				continue
+			filepath = f"{root}/{file}"
 
-		# Load image
-		image = Image.open(filepath)
-		image = image.convert("RGBA")
-		pixels = image.load()
-		width, height = image.size
+			# Load image
+			image = Image.open(filepath)
+			image = image.convert("RGBA")
+			pixels = image.load()
+			width, height = image.size
 
-		# Optimize image
-		for x in range(width):
-			for y in range(height):
-				r, g, b, a = pixels[x, y]
-				if a == 0:
-					pixels[x, y] = (0, 0, 0, 0)
-		
-		# Save image
-		image.save(filepath)
-		info(f"Optimized '{file}'")
+			# Optimize image
+			for x in range(width):
+				for y in range(height):
+					r, g, b, a = pixels[x, y]
+					if a == 0:
+						pixels[x, y] = (0, 0, 0, 0)
 
-# Total time
-total_time = time.perf_counter() - START_TIME
-info(f"Textures optimized in {total_time:.3f} seconds")
+			# Save image
+			image.save(filepath)
+			stp.info(f"Optimized '{file}'")
 
